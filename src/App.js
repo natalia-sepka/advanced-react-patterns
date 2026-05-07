0e566ac5d98e0a5ad3985e8ce12c31df84005e01
natalia.sepka <natalia.sepka@gmail.com>
2026-05-07 21:36:19 +0200

    recursive component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { UserInfo } from "./components/user-info";
-import { BookInfo } from "./components/book-info";
+import { RecursiveComponent } from "./components/recursive";
+
+const myNestedObject = {
+    key1: "value",
+    key2: {
+        innerKey1: "innerValue1",
+        innerKey2: {
+            innerInnerKey1: "innerInnerKey1" ,
+            innerInnerKey2: "innerInnerKey2" ,
+        }
+    },
+    key3: "value3"
+}
 
 function App() {
 
   return (
    <>
-       <UserInfo userId={"1"} />
-       <BookInfo bookId="2" />
+       <RecursiveComponent data={myNestedObject} />
    </>
   );
 }
