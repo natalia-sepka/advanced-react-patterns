e3fcb0988b2671e0ab7a44f876e4d9cbd5afad47
natalia.sepka <natalia.sepka@gmail.com>
2026-05-08 16:17:28 +0200

    compound components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { SmallRedButton, RedButton } from "./components/partial";
+import Card from "./components/card";
 
 function App() {
 
   return (
    <>
-       <RedButton text={"I'm a red button"} />
-       <SmallRedButton text={"I'm a small and green button"} />
+       <Card>
+           <Card.Header>
+                <h1 style={{ margin: "0" }}>Header</h1>
+           </Card.Header>
+           <Card.Body>
+               Some random text Some random text Some random text Some random text Some random text
+               Some random text Some random text Some random text Some random text Some random text
+               Some random text Some random text Some random text Some random text Some random text
+           </Card.Body>
+           <Card.Footer>
+               <button>Ok</button>
+               <button>Cancel</button>
+           </Card.Footer>
+       </Card>
    </>
   );
 }
