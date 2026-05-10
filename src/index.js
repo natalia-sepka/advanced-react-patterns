255d8a5c1568ad2e8081ba1814b442aebbcc6341
natalia.sepka <natalia.sepka@gmail.com>
2026-05-10 21:14:10 +0200

    error boundaries fix

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ErrorBoundry } from "./components/error-boundary";
+import { ErrorBoundary } from "./components/error-boundary";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <ErrorBoundry fallback={<h1>Error at App level</h1>}>
+      <ErrorBoundary fallback={<h1>Error at App level</h1>}>
           <App />
-      </ErrorBoundry>
+      </ErrorBoundary>
   </React.StrictMode>
 );
 
