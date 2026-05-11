da060f9fa637c59df8cfe012e417913138b9fb36
natalia.sepka <natalia.sepka@gmail.com>
2026-05-11 20:53:04 +0200

    useId

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import mitt from "mitt";
-import { useLayoutEffect, useRef, useState} from "react";
+import { Form } from "./components/form";
 
 export const emitter = mitt();
 
 function App() {
-    const [show, setShow] = useState(false);
-    const [top, setTop] = useState(0);
-    const buttonRef= useRef(null);
-
-    useLayoutEffect(() => {
-        if (buttonRef.current === null || !show) return setTop(0);
-        const { bottom } = buttonRef.current.getBoundingClientRect();
-        setTop(bottom + 30)
-    }, [show]);
 
   return (
     <>
-        <button ref={buttonRef} onClick={() => setShow((s) => !s)}>Show</button>
-        <br />
-        {show && (
-            <div className="tooltip"
-                style = {{
-                    top: `${top}px`
-                }}
-            >
-                Some text...
-            </div>
-        )}
+        <Form />
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout</p>
+        <Form />
     </>
   );
 }
