0d03510b13e39281f3b0789f19a78fa7b9c5835e
natalia.sepka <natalia.sepka@gmail.com>
2026-05-26 20:26:58 +0200

    updating data with react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import FetchTopQuotes from "./components/top-quotes";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
+import { UpdateQuotes } from "./components/update-code";
+import FetchTopQuotes from "./components/top-quotes";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+        <ToastContainer />
+        <UpdateQuotes />
         <FetchTopQuotes />
     </QueryClientProvider>
   );
--- src/server/src/index.js
+++ src/server/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { quotes } = require("./quotes");
 const { quotesOriginal } = require("./quotesOriginal");
-const nanoid = require("nanoid");
+const { nanoid } = require("nanoid");
 const cors = require("cors");
 const app = express();
 const port = 9000;
